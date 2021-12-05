4fdc21cd0ca9633f6061f0b3ac7e922b0b825a42
Redouane Bekkouche <redouane.bekk@gmail.com>
2021-12-05 10:37:03 +0100

    installing firebase and setup envirenments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAWW3RAmqZsTMr1I-mkgFp-NL7qoTMxtBU",
+        authDomain: "ngauth-d9be6.firebaseapp.com",
+        projectId: "ngauth-d9be6",
+        storageBucket: "ngauth-d9be6.appspot.com",
+        messagingSenderId: "80755048338",
+        appId: "1:80755048338:web:86db9dd52a7b2bcfba4b4b",
+        measurementId: "G-B0TT7KB705"
+    }
 };
 
 /*
