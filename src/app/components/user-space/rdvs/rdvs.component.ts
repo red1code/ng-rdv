904306b36f3ac8a5c6e5372c68b4f24a9cee8db4
Redouane Bekkouche <redouane.bekk@gmail.com>
2021-12-10 19:04:22 +0100

    working on rdvs component

--- src/app/components/user-space/rdvs/rdvs.component.ts
+++ src/app/components/user-space/rdvs/rdvs.component.ts
@@ -1,15 +1,95 @@
+import { Rendezvous } from './../../../models/rendezvous';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { DatabaseService } from 'src/app/services/database.service';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-rdvs',
-  templateUrl: './rdvs.component.html',
-  styleUrls: ['./rdvs.component.css']
+    selector: 'app-rdvs',
+    templateUrl: './rdvs.component.html',
+    styleUrls: ['./rdvs.component.css']
 })
 export class RdvsComponent implements OnInit {
 
-  constructor() { }
+    id: string = '';
+    rdv!: Rendezvous;
+    rdvForm: FormGroup;
+    rdvsList!: Observable<Rendezvous[]>;
+    firebaseErrorMessage!: string;
+    tHead: string[] = ['Order', 'Full Name', 'Phone Number', 'Created At', 'Last update'];
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private dtbaseService: DatabaseService
+    ) {
+        this.rdvForm = this.formBuilder.group({
+            displayName: ['', [Validators.required, Validators.pattern(/.*\S.*/)]],
+            phoneNumber: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]]
+        });
+    }
 
+    ngOnInit(): void {
+        this.getRDVsList();
+    }
+
+    getRDVsList() {
+        this.rdvsList = this.dtbaseService.getRDVsList();
+    }
+
+    onSubmitForm() {
+        if (this.rdvForm.invalid) return;
+        this.rdv = this.rdvForm.value;
+        if (this.id === '') {
+            this.rdv.created_at = new Date();
+            this.rdv.created_by = this.authService.currentUserProfile.email;
+            this.dtbaseService.createNewRDV(this.rdv)
+            this.rdvForm.reset();
+        } else {
+            this.rdv.lastUpdate = new Date();
+            this.dtbaseService.updateRDV(this.id, this.rdv);
+            this.rdvForm.reset();
+            this.id = '';
+        }
+    }
+
+    onUpdateIcon(rdv: any) {
+        this.rdvForm = this.formBuilder.group({
+            displayName: [rdv.displayName, [Validators.required]],
+            phoneNumber: [rdv.phoneNumber, Validators.required]
+        });
+        this.id = rdv.rdvID;
+    }
+
+    emptyList() {
+        let list = this.rdvsList as unknown as Array<any>;
+        if (list.length === 0) return true;
+        else return false;
+    }
+
+    onDelete = (data: any) => this.dtbaseService.deleteRDV(data);
+
+    checkUserPermission(rdv: any): boolean {
+        let role = this.authService.currentUserProfile.role;
+        let userEmail = this.authService.currentUserProfile.email;
+        let rdvEmail = rdv.created_by;
+        if (userEmail === rdvEmail || role === 'admin' || role === 'editor') return true;
+        else return false;
+    }
+
+    resetForm() {
+        this.id = '';
+        this.rdvForm.reset();
+    }
 }
+
+// THE END.
+
+
+
+/*
+
+datePipe: string = 'MMMM d, y - hh:mm aa';
+
+*/
--- src/app/components/user-space/user-space.module.ts
+++ src/app/components/user-space/user-space.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserSpaceRoutingModule } from './user-space-routing.module';
 import { UserSpaceComponent } from './user-space.component';
 import { RdvsComponent } from './rdvs/rdvs.component';
@@ -15,7 +15,9 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     CommonModule,
-    UserSpaceRoutingModule
+    UserSpaceRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class UserSpaceModule { }
